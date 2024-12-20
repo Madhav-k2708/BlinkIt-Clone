b0d643ce961b70f448d0354c7d8f394a90fe946a
Madhav-k2708 <madhav.k714@gmail.com>
2024-12-20 20:28:08 +0530

    second commit

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,5 @@
 import sendEmail from "../config/sendEmail.js";
-import userModel from "../models/userModel.js/";
+import userModel from "../models/userModel.js";
 import bcrypt from "bcryptjs";
 import validator from "validator";
 import jwt from "jsonwebtoken";
