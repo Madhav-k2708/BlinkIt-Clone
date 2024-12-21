8d19e8beab1237d3a71cf32320fd2b436e0c1d78
Madhav-k2708 <madhav.k714@gmail.com>
2024-12-21 18:29:58 +0530

    third commit

--- server/server.js
+++ server/server.js
@@ -17,7 +17,12 @@ import orderRouter from "./route/orderRoute.js";
 
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
